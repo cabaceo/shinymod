84a23d573724c7876a5d5d9ac44392172cdfd9ad
Guangming Lang <rigor6vigor@gmail.com>
2020-09-10 16:19:04 +0900

    add one more example of how to make horizontal bar chart

--- inst/examples/ex-mk-barploth-resp.R
+++ inst/examples/ex-mk-barploth-resp.R
@@ -9,3 +9,11 @@ f('Sepal.Length', 'Species')
 f('Sepal.Length', 'status')
 f('Sepal.Length', 'status', fillby = 'Species')
 f('Sepal.Length', 'Species', fillby = 'status')
+
+
+
+df = ggplot2::diamonds %>% count(clarity) %>% mutate(pct = n / sum(n))
+g = mk_barploth_resp(df)
+g("n", "clarity", label_decimals = 0)
+g("pct", "clarity", label_decimals = 2)
+g("pct", "clarity", show_pct = T)
