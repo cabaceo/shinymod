a17ad866a1712a45279023d2de3a8ad8b3ebc3e9
Guangming Lang <rigor6vigor@gmail.com>
2023-08-31 08:00:40 +0900

    comment off header regex cleaning. User need to make sure file header is nice before uploading.

--- R/upload.R
+++ R/upload.R
@@ -48,7 +48,7 @@ upload_server = function(id) {
                         df = readr::read_delim(user_file()$datapath,
                                                delim = input$sep,
                                                col_names = input$heading)
-                        names(df) = gsub('\x92', '', names(df))
+                        # names(df) = gsub('\x92', '', names(df)) # https://blog.r-project.org/2022/06/27/why-to-avoid-%5Cx-in-regular-expressions/index.html
                         df
                 })
         })
